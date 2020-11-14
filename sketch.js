a73067280fe0cfc4513dabd64fee15506c1a1104
SahilSharma12 <73352525+SahilSharma12@users.noreply.github.com>
2020-11-14 15:43:43 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,27 +1,60 @@
+
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
+const Body = Matter.Body;
 
-var engine, world;
-var box1;
+var paper1;
+var ground1;
+var dust1,dust2,dust3;
+var wall;
 
-function setup(){
-    var canvas = createCanvas(400,400);
-    engine = Engine.create();
-    world = engine.world;
-
-    box1 = new Box(200,300,50,50);
-    box2 = new Box(240,100,50,100);
-    ground = new Ground(200,height,400,20)
+function preload()
+{
+wall=loadImage("office.png")	
 }
 
-function draw(){
-    background(0);
-    Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
-    box1.display();
-    box2.display();
-    ground.display();
+
+
+function setup() {
+	createCanvas(900, 400);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	wall.scale = 2
+	//Create the Bodies Here.
+	paper1 = new Paper(100,310,20);
+	ground1 = new Ground(width/2,390,900,10)
+	dust3 = new Dustbin(700,380,200,15)
+	dust1 = new Dustbin(600,330,15,100)
+	dust2 = new Dustbin(800,330,15,100)
+
+	Engine.run(engine);
+  
 }
+
+
+function draw() {
+ 
+  background(wall);
+  ellipseMode(RADIUS);
+
+  paper1.display();
+  ground1.display();
+  dust1.display();
+	dust2.display();
+  dust3.display();
+  drawSprites();
+ 
+}
+	function keyPressed(){
+		if(keyCode===UP_ARROW){
+		Matter.Body.applyForce(paper1.body, paper1.body.position, {x:65,y:-65 })
+		}
+
+
+
+	}
+
+
